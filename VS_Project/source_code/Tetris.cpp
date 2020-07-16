48bf41e21a80fd8b391168417f09a2beede848e6
aveholow <marcin444555vp@gmail.com>
2020-07-16 17:40:21 +0200

    e1.0

---
+++ VS_Project/source_code/Tetris.cpp
@@ -0,0 +1,28 @@
+#include "pch.h"
+
+
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(1920, 1080), "SFML works!", sf::Style::Fullscreen);
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape)
+				window.close();
+			
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
+	return 0;
+}
---
+++ VS_Project/source_code/pch.h
@@ -0,0 +1,17 @@
+#define NOMINMAX
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <tuple>
+
+
+	#include <Windows.h>
+
+
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Main.hpp>
+#include <SFML/Network.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
